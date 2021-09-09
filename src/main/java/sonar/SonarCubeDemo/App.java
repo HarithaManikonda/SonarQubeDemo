e6ae79906c834937cbf87c0facd79ed6c67f294e
Haritha Manikonda <haritha.manikonda@gmail.com>
2021-09-09 13:56:04 -0700

    First commit

---
+++ src/main/java/sonar/SonarCubeDemo/App.java
@@ -0,0 +1,13 @@
+package sonar.SonarCubeDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
